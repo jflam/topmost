ffeafa0f50104cfe45d88ef34e1cd303c07d6fca
unknown <jflam@.redmond.corp.microsoft.com>
2008-11-27 02:11:36 -0800

    created a simple floating toolbox window

---
+++ stream.rb
@@ -0,0 +1,18 @@
+
+require 'PresentationCore'
+require 'PresentationFramework'
+include System::Windows
+include System::Windows::Controls
+w = Window.new
+w.Topmost = true
+w.WindowStyle = WindowStyle.ToolWindow
+w.Show
+w.Title = "hi mom!"
+s = StackPanel.new
+b = Button.new
+b.Content = "Click Me!"
+s.Children.Add b
+w.Content = s
+b.Click do |sender, args|
+  MessageBox.show "clicked me!"
+end
