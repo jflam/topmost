f3a3886a82efb0adb41686d979189738f0cd5750
unknown <jflam@.redmond.corp.microsoft.com>
2008-11-27 02:42:51 -0800

    made it into a function

--- stream.rb
+++ stream.rb
@@ -1,20 +1,24 @@
 
 require 'PresentationCore'
 require 'PresentationFramework'
+
 include System::Windows
 include System::Windows::Controls
 include System::Windows::Media
-w = Window.new
-w.Topmost = true
-w.WindowStyle = WindowStyle.ToolWindow
-w.Title = "hi mom!"
-s = StackPanel.new
-b = Button.new
-b.Content = "Click Me!"
-s.Children.Add b
-w.Content = s
-b.Click do |sender, args|
-  MessageBox.show "clicked me!"
+
+def create_topmost_window
+  w = Window.new
+  w.Topmost = true
+  w.WindowStyle = WindowStyle.ToolWindow
+  w.Title = "hi mom!"
+  s = StackPanel.new
+  b = Button.new
+  b.Content = "Click Me!"
+  s.Children.Add b
+  w.Content = s
+  b.Click do |sender, args|
+    MessageBox.show "clicked me!"
+  end
+  w.Background = Brushes.Black
+  w
 end
-w.Background = Brushes.Black
-w.Show
